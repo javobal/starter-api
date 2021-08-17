e26a74f024703d4ba229c75368a7bd9ed42099ca
Javier Balam <javier.balam@gmail.com>
2021-08-16 22:18:39 -0500

    error and validation handling

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import express from "express";
 import morgan from "morgan";
 import {RegisterRoutes} from "./routes/routes";
 import initFirebase from "./lib/firebase";
 import swaggerUi from "swagger-ui-express";
+import express, {
+  Response as ExResponse,
+  Request as ExRequest,
+  NextFunction,
+} from "express";
+import { ValidateError } from "tsoa";
 initFirebase()
 
 const app = express();
@@ -22,6 +27,34 @@ app.use(
 );
 
 RegisterRoutes(app)
+
+app.use(function notFoundHandler(_req, res: ExResponse) {
+  res.status(404).send({
+    message: "Not Found",
+  });
+});
+
+app.use(function errorHandler(
+  err: unknown,
+  req: ExRequest,
+  res: ExResponse,
+  next: NextFunction
+): ExResponse | void {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
+  if (err instanceof Error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+
+  next();
+});
 
 
 
