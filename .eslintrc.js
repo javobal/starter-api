e81bc0f57c609d1a5934541ef4c1577b3283e3fe
Javier Balam <javier.balam@gmail.com>
2022-03-15 23:51:40 -0600

    eslint cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         es2021: true,
         node: true,
     },
-    extends: ['eslint:recommended', 'plugin:node/recommended', 'prettier'],
+    extends: ['eslint:recommended', 'prettier'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 'latest',
@@ -11,6 +11,5 @@ module.exports = {
     },
     plugins: ['@typescript-eslint'],
     rules: {
-        'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }], // Tem fix for node/no-unsupported-festures/es-syntax
-    },
+    }
 }
